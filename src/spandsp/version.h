cc8d574bd0659975ea9e069f79f898a1d9d7c33c
Steve Underwood <steveu@coppice.org>
2011-01-22 09:49:54 +0200

    importing spandsp 0.0.6pre18 (same as snapshot 20110122)

--- src/spandsp/version.h
+++ src/spandsp/version.h
@@ -28,9 +28,9 @@
 
 /* The date and time of the version are in UTC form. */
 
-#define SPANDSP_RELEASE_DATE            20110102
-#define SPANDSP_RELEASE_TIME            103746
-#define SPANDSP_RELEASE_DATETIME_STRING "20110102 103746"
+#define SPANDSP_RELEASE_DATE            20110122
+#define SPANDSP_RELEASE_TIME            075024
+#define SPANDSP_RELEASE_DATETIME_STRING "20110122 075024"
 
 #endif
 /*- End of file ------------------------------------------------------------*/
